ed31f92b3f8c3b51de1fa49fbfa0accba28d0f76
jtkaufman737 <jtkaufman737@gmail.com>
2016-06-27 11:30:04 -0400

    Pseudo-Coding steps
    Pseudo coding what I need to do

---
+++ app.js
@@ -0,0 +1,15 @@
+//onload shows board, prompts for player one and player two name
+//
+
+
+//GAME LOGIC
+//If an odd turn, onclick set div to x
+//If an even turn, onclick set div to o
+//On all turns, set a status of clicked to tile (so it can't be reclicked)
+//After click run a function evaluating if there are three tiles of the same type in a row
+//If the answer is yes, run an alert with the winner
+
+//Reset button puts all the tiles back to having no value, and ideally if I can get player names populating 
+//resets the prompt asking for their names again
+
+//
