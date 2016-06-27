cdd56ad4116dc1b6ae88ff376d10f57863592d18
jtkaufman737 <jtkaufman737@gmail.com>
2016-06-27 13:11:06 -0400

    Got name prompts working and displayed to variables

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
 
+var player1 = prompt('What is your name?', 'type name'); //got working & saved to variable
+var player2 = prompt('Hey, how bout you?', 'type name'); //got working & saved to variable
 
-var start = function () {
-	startbutton.onclick{
-		document.querySelector
-	}
-	//onload prompts for names
-	//runs setnames function which prompts for names, then displays them
-	//makes sure innerHTML for the squares is set to ''
+// var start = function () {
+		
+// 	}
 
-var settingUpTileClasses = function ()
-	//after start button is hit, will grab them by row or column
-	//then need a way to set different ids
+// start();
+// 	//onload prompts for names
+// 	//runs setnames function which prompts for names, then displays them
+// 	//makes sure innerHTML for the squares is set to ''
+
+// var settingUpTileClasses = function ()
+// 	//after start button is hit, will grab them by row or column
+// 	//then need a way to set different ids
 
 
-var setNames = function () {
-	//prompt: gets names
-	//gets element by id, name blurbs or whatever I end up calling them
-	//sets blurb inner html to be what the prompt returned
-}
+// var setNames = function () {
+// 	//prompt: gets names
+// 	//gets element by id, name blurbs or whatever I end up calling them
+// 	//sets blurb inner html to be what the prompt returned
+// }
 
-var evenAndOddTurns = function () {
-	//say for clicks on a square, the first time it will be an x, second time it will be an o
-	//need to dig into the logic here, I'm not sure how I would do this
-}
+// var evenAndOddTurns = function () {
+// 	//say for clicks on a square, the first time it will be an x, second time it will be an o
+// 	//need to dig into the logic here, I'm not sure how I would do this
+// }
 
-var checkingTheScore = function(){
-	// checks if there is  a win
-	//if else statement, if win = set up win sequence of events, else {nuttin}
-	// if win is selected, trigger win function
+// var checkingTheScore = function(){
+// 	// checks if there is  a win
+// 	//if else statement, if win = set up win sequence of events, else {nuttin}
+// 	// if win is selected, trigger win function
 
-var winnerWinner = function() {
-	//change color of winners name console box
-	//generates some type of alert
-	//Should ideally make any other tiles on the board unclickable, but I'm not sure how to do that 
-}
+// var winnerWinner = function() {
+// 	//change color of winners name console box
+// 	//generates some type of alert
+// 	//Should ideally make any other tiles on the board unclickable, but I'm not sure how to do that 
+// }
